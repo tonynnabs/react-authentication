378ba48b373fbf3bd81ba4c9a9ea1b77af223e96
tonynnabs <tonynnabs01@gmail.com>
2022-04-13 18:05:15 -0400

    adding user signup

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,50 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-
+  const emailRef = useRef();
+  const passwordRef = useRef();
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler= (e) => {
+    e.preventDefault();
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoCMcs6Yg_kIuIk_lKb7_QTnqFJBRgelE', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      // console.log(res.json());
+      if(res.ok){
+        //...
+      }else{
+        res.json().then((data) => {
+          console.log(data);
+        })
+      }
+      
+    })
+  }
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input ref={emailRef} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input ref={passwordRef} type='password' id='password' required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
