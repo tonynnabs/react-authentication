e187e4eb067bedca306783d68c93d65e438da063
tonynnabs <tonynnabs01@gmail.com>
2022-04-13 18:49:55 -0400

    using context

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,59 +1,74 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const authCtx = useContext(AuthContext)
   const emailRef = useRef();
   const passwordRef = useRef();
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-
-  const submitHandler= (e) => {
+  const submitHandler = (e) => {
     e.preventDefault();
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoCMcs6Yg_kIuIk_lKb7_QTnqFJBRgelE', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-        returnSecureToken: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((res) => {
-      // console.log(res.json());
-      if(res.ok){
-        //...
-      }else{
-        res.json().then((data) => {
-          console.log(data);
-        })
-      }
-      
-    })
-  }
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDoCMcs6Yg_kIuIk_lKb7_QTnqFJBRgelE';
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoCMcs6Yg_kIuIk_lKb7_QTnqFJBRgelE';
+    }
+      fetch(url,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMsg = "Authentication failed";
+            if (data && data.error && data.error.message) {
+              errorMsg = data.error.message;
+            }
+          throw new Error(errorMsg);
+          });
+        }
+      }).then((data) => {
+        authCtx.login(data.idToken)
+      }).catch(err => {
+        alert(err.message);
+      });
+  };
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input ref={emailRef} type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailRef} type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input ref={passwordRef} type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
